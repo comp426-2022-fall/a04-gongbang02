476399fb0fe24e9f6956e5689af5672140e9aa4f
Bang Gong <gongbang@email.unc.edu>
2022-11-28 16:50:12 -0500

    in progress

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.post('/app/roll/', (req, res) => {
 	const sides = parseInt(req.body.sides);
 	const dice = parseInt(req.body.dice);
 	const rolls = parseInt(req.body.rolls);
-	res.send(roll(sides, dice, rolls));
+	res.status(200).send(roll(sides, dice, rolls));
 });
 
 app.get('/app/roll/:sides/', (req, res) => {
